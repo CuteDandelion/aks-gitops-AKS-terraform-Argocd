f840e2e3202d863af036a87c160a93c2c7e3fc10
Hanad Isa <Hanadisa05@gmail.com>
2025-07-11 20:49:31 +0100

    add backend.tf for remote state management

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-aks-kanban"
+    storage_account_name = "rmttfstatehanad"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}

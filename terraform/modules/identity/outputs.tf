56891220a2c75cde6e33e1a532d1796e5d18766f
Hanad Isa <Hanadisa05@gmail.com>
2025-07-12 21:18:48 +0100

    Testing TF Plan Pipeline

--- terraform/modules/identity/outputs.tf
+++ terraform/modules/identity/outputs.tf
@@ -1,9 +1,9 @@
 output "identity_id" {
   description = "Managed Identity resource ID"
-  value       = azurerm_user_assigned_identity.this.id
+  value       = azurerm_user_assigned_identity.identity.id
 }
 
 output "principal_id" {
   description = "Managed Identity principal (object) ID"
-  value       = azurerm_user_assigned_identity.this.principal_id
+  value       = azurerm_user_assigned_identity.identity.principal_id
 }

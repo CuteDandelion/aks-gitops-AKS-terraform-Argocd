8ab55fd741b2e996fd7586517a27983f3884a57d
Hanad Isa <Hanadisa05@gmail.com>
2025-07-15 17:02:42 +0100

    Testing TF Apply Pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,17 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
   backend "azurerm" {
-    resource_group_name  = "rg-aks-kanban"
-    storage_account_name = "rmttfstatehanad"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.backend_key
+    subscription_id      = var.subscription_id
+    tenant_id            = var.tenant_id
   }
 }

7c9bf916ca7ad8fb221833b9e301388ffd18bfca
Hanad Isa <Hanadisa05@gmail.com>
2025-07-16 22:48:16 +0100

    Correct helm provider syntax with nested kubernetes block

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,11 +1,14 @@
-provider "helm" {
-  kubernetes { 
-    host                   = module.aks.aks_host
-    client_key             = module.aks.aks_client_key
-    client_certificate     = module.aks.aks_client_certificate
-    cluster_ca_certificate = module.aks.aks_cluster_ca_certificate
+provider "kubernetes" {               # Kubernetes provider
+  alias       = "main"
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {                     # Helm Provider
+  kubernetes {
+    config_path = "~/.kube/config"
   }
 }
+
 
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"

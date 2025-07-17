e0e7bd0c7f65c825b36e6f9f1adfecca678918c4
Hanad Isa <Hanadisa05@gmail.com>
2025-07-17 16:10:52 +0100

    cert manager config

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,12 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host                   = module.aks.aks_host
-    client_key             = module.aks.aks_client_key
-    client_certificate     = module.aks.aks_client_certificate
-    cluster_ca_certificate = module.aks.aks_cluster_ca_certificate
-  }
-}
-
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"

083eafc587e5c367c848df06229d99bcc73dff25
justinchin <justinchin.professionalcareer@gmail.com>
2025-10-14 17:52:05 -0400

    disable tfstate in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,11 +88,11 @@ data "kubernetes_service" "nginx_ingress" {
 
 
 
-module "tfstate" {
-  source               = "./modules/storage"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  storage_account_name = var.storage_account_name
-  container_name       = var.container_name
-}
+#module "tfstate" {
+#  source               = "./modules/storage"
+#  resource_group_name  = var.resource_group_name
+#  location             = var.location
+#  storage_account_name = var.storage_account_name
+#  container_name       = var.container_name
+#}
 

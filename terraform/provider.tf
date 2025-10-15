31c03d1f0ab75e0a46b12fa8ca16fffbe178c757
justinchin <justinchin.professionalcareer@gmail.com>
2025-10-15 19:54:55 -0400

    fix destroy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,10 +7,11 @@ provider "azurerm" {
 
 provider "helm" {
   kubernetes {
-    host                   = module.aks.aks_host
-    client_certificate     = module.aks.aks_client_certificate
-    client_key             = module.aks.aks_client_key
-    cluster_ca_certificate = module.aks.aks_cluster_ca_certificate
+    host                   = try(module.aks.aks_host, null)
+    client_certificate     = try(module.aks.aks_client_certificate, null)
+    client_key             = try(module.aks.aks_client_key, null)
+    cluster_ca_certificate = try(module.aks.aks_cluster_ca_certificate, null)
+    config_path            = "/home/runner/.kube/config"
   }
 }
 

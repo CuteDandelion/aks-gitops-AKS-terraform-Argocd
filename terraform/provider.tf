ef29ff5da5188862db1a737e9fd31c89fe5bdcc6
justinchin <justinchin.professionalcareer@gmail.com>
2025-10-15 20:02:07 -0400

    fix destroy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,11 +17,12 @@ provider "helm" {
 
 
 provider "kubectl" {
-  host                   = module.aks.aks_host
-  client_certificate     = module.aks.aks_client_certificate
-  client_key             = module.aks.aks_client_key
-  cluster_ca_certificate = module.aks.aks_cluster_ca_certificate
-  load_config_file       = false
+  host                   = try(module.aks.aks_host, null)
+  client_certificate     = try(module.aks.aks_client_certificate, null)
+  client_key             = try(module.aks.aks_client_key, null)
+  cluster_ca_certificate = try(module.aks.aks_cluster_ca_certificate, null)
+  config_path            = "/home/runner/.kube/config"
+  load_config_file       = true
 }
 
 
